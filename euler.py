880a63b71e78bf0b8c7b9b3ceab4228fcf33688f
Sylvain <sylvain.desodt+github@gmail.com>
2016-02-13 11:16:14 +0000

    Fix style

--- euler.py
+++ euler.py
@@ -149,14 +149,14 @@ def euler17(lim=1000):
     # prefixes/suffixes and use multiplications instead of
     # repeated sums but this is fast enough
     small_numbers_str = {
-        1: "one", 2: "two", 3:"three", 4:"four", 5:"five", 6:"six",
-        7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven",
-        12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen",
-        16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen"
+        1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six",
+        7: "seven", 8: "eight", 9: "nine", 10: "ten", 11: "eleven",
+        12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen",
+        16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"
     }
     ten_multiples_str = {
-        2:"twenty", 3:"thirty", 4:"forty", 5:"fifty",
-        6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety"
+        2: "twenty", 3: "thirty", 4: "forty", 5: "fifty",
+        6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"
     }
     small_numbers_len = {k:len(v) for k, v in small_numbers_str.items()}
     ten_multiples_len = {k:len(v) for k, v in ten_multiples_str.items()}
@@ -178,11 +178,12 @@ def euler17(lim=1000):
         if tens > 1:
             total += ten_multiples_len[tens]
         else:
-            units += 10 * tens # move tens in units
+            units += 10 * tens  # move tens in units
         if units:
             total += small_numbers_len[units]
         return total
     return sum(get_len_for_number(i) for i in range(1, lim + 1))
+
 
 def euler18_():
     """Solution for problem 18."""
